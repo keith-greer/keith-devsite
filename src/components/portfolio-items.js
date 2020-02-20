4b2c3892b27a32fedee456388ba83506d0b40125
Keith Greer <54289218+keith-greer@users.noreply.github.com>
2020-02-20 14:45:35 +0000

    Portfolio amends

--- src/components/portfolio-items.js
+++ src/components/portfolio-items.js
@@ -3,7 +3,7 @@ const portfolioItems = [
         title: 'Simply Thank You',
         sinopsis: 'A standard corporate web design...with a twist.',
         desc: 'This site is built based upon a fairly standard B2B design but I had to incorporate a bespoke CMS so I could then implement a 10,000+ product range and ecommerce function. Languages/Libraries used: HTML, CSS, vanilla JavaScript and jQuery.',
-        img: './portfolio/faduense.png',
+        img: './portfolio/sty.png',
         live: 'https://www.simplythankyou-corporate.com/',
         repo: 'https://www.simplythankyou-corporate.com/',
         idx: 1
@@ -12,18 +12,18 @@ const portfolioItems = [
         title: 'Keefusoutdoors',
         sinopsis: 'A simple Wordpress Blog.',
         desc: 'This is one of my first forays into web development. I rarely add content to this now but I have kept it as a `playground of sorts to keep up to date with the WP Eco system.`',
-        img: './portfolio/bitcoin.png',
+        img: './portfolio/keefusoutdoors.png',
         live: 'https://www.keefusoutdoors.co.uk/',
         repo: 'https://www.keefusoutdoors.co.uk/',
         idx: 2
     },
     {
-        title: 'Studio 212',
-        sinopsis: 'This is a simple, brochure style site leveraging React, Gatsby and GraphQL',
-        desc: 'Write posts directly in markdown. Thanks to plugins, and the createPage Api, gatbsy generates blog posts automatically',
+        title: 'keithpgreer.com',
+        sinopsis: 'This is a simple, portfolio style site leveraging React, Gatsby and GraphQL stack',
+        desc: 'React built static website with full featured blog andx portfolio projects accordion on the index page. Write blog posts directly in markdown. Gatsby is configured to then auto generate blogposts based off the markdown files created.',
         img: './portfolio/ejs-blog.png',
         repo: 'https://github.com/martin2844/ejs-blog',
-        live: 'https://my-ejs-blog.herokuapp.com/',
+        live: 'https://www.keithpgreer.com',
         idx: 3
     },
     {
@@ -34,16 +34,8 @@ const portfolioItems = [
         repo: 'https://github.com/martin2844',
         live: 'https://my-ejs-todo-list.herokuapp.com',
         idx: 4
-    },
-    {
-        title: 'Portfolio Item 5',
-        sinopsis: 'Please message me at twitter @codigoMate',
-        desc: 'I really appriciate any messages',
-        img: './portfolio/Mate.png',
-        repo: 'https://github.com/martin2844',
-        live: 'https://codigomate.com',
-        idx: 5
     }
+
 
 ]
 
